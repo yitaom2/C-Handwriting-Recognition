954e94b33cb618170be6a462bafc7de27ce705f0
Yitao Meng <yitaom2@illinois.edu>
2019-07-13 04:52:11 -0500

    a

--- combine.c
+++ combine.c
@@ -291,20 +291,20 @@ double * getBias(char * layerName, int num) {
 void image_recognition(int imagenum) {
   double *** image = createImage(imagenum);
 
-  int weightDim1[3] = {16, 3, 3}; int layerDim1[3] = {1, 28, 28};
+  int weightDim1[3] = {16, 4, 4}; int layerDim1[3] = {1, 28, 28};
   int maskDim2[3] = {2, 2}; int layerDim2[3] = {16, 28, 28};
   int layerDim3[3] = {16, 14, 14};
   int weightDim4[2] = {3136, 20};
   int weightDim5[2] = {20, 10};
 
-  int revised_weightDim1[4] = {16, 1, 3, 3};
-  double **** weight1 = ConvWeight("Conv1", revised_weightDim1);
-  double ** weight4 = DenseWeight("Dense1", weightDim4);
-  double ** weight5 = DenseWeight("Dense2", weightDim5);
+  int revised_weightDim1[4] = {16, 1, 4, 4};
+  double **** weight1 = ConvWeight("conv2d_1", revised_weightDim1);
+  double ** weight4 = DenseWeight("dense_1", weightDim4);
+  double ** weight5 = DenseWeight("dense_2", weightDim5);
 
-  double * bias1 = getBias("Conv1", 16);
-  double * bias4 = getBias("Dense1", 20);
-  double * bias5 = getBias("Dense2", 10);
+  double * bias1 = getBias("conv2d_1", 16);
+  double * bias4 = getBias("dense_1", 20);
+  double * bias5 = getBias("dense_2", 10);
 
   double *** layerConv1 = convolution2D(image, weight1, layerDim1, weightDim1, bias1, 1, "SAME");
   double *** layerPool = MaxPooling2D(layerConv1, layerDim2, maskDim2, 2, "SAME");
@@ -312,25 +312,35 @@ void image_recognition(int imagenum) {
   double * layerDense1 = Dense(layerFlatten, weight4, bias4, 3136, 20, "relu");
   double * layerDense2 = Dense(layerDense1, weight5, bias5, 20, 10, "softmax");
 
-  printf("HERE ARE THE RESULT:\n");
+  printf("THE RESULT FOR IMAGE %d: \n", imagenum);
   for (int i = 0; i < 10; i++) {
     printf("%20.15f ", layerDense2[i]);
   }
   printf("\n");
+  printf("---------------------------------------------------\n");
+  // for (int i = 0; i < 16; i++) {
+  //   printf("LAYER%d\n", i);
+  //   for (int j = 0; j < 28; j++) {
+  //     for (int k = 0; k < 28; k++) {
+  //       printf("%20.15f ", layerConv1[i][j][k]);
+  //     }
+  //     printf("\n");
+  //   }
+  // }
 }
 
 int main(int argc, char * argv[]) {
   if (argc <= 1) {
     printf("enter in the format: ./combine.o [number in range[0, 10000)]\n");
+    printf("or to get several result at the same time: './combine.o num1 num2 ...'\n");
     exit(0);
   }
-  int imagenum = atoi(argv[1]);
-  if (imagenum == 0) {
-    printf("currently showing the result of image 0\n");
-    printf("you either entered 0 as the image number or entered a none number variable\n");
-    printf("enter in the format: ./combine.o [number in range[0, 10000)]\n");
-    printf("----------------------------------------------------------\n");
+  for (int i = 1; i < argc; i++) {
+    int imagenum = atoi(argv[i]);
+    if (imagenum == 0) {
+      printf("currently showing the result of image 0: ");
+    }
+    image_recognition(imagenum);
   }
-  image_recognition(imagenum);
   return 0;
 }
